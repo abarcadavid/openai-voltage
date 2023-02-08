920c85559ce8c13aaecb7bb22adc5fc499b17da1
abarcadavid <davidabarca.swe@protonmail.com>
2023-02-08 10:49:38 -0800

    Add textarea box and button

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import {useState} from 'react';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+
+  const onUserChangedText = (e) => {
+    console.log(e.target.value);
+    setUserInput(e.target.value);
+  }
+
   return (
     <div className="root">
       <Head>
@@ -11,10 +19,24 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Voltage</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Your high power microwave assistant</h2>
+          </div>
+        </div>
+        <div className='prompt-container'>
+          <textarea
+            placeholder="start typing here"
+            className='prompt-box'
+            value={userInput}
+            onChange={(e) => onUserChangedText(e)}/>
+          <div className='prompt-buttons'>
+            <a className='generate-button' onClick={null}>
+              <div className='generate'>
+                <p>Generate</p>
+              </div>
+            </a>
           </div>
         </div>
       </div>
