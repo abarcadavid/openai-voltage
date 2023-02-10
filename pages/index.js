b1c94f813504d166eb19550b3d16760e79fbb2f6
abarcadavid <davidabarca.swe@protonmail.com>
2023-02-09 16:03:20 -0800

    Add function that hits our generate endpoint

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,34 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
 import {useState} from 'react';
 
 const Home = () => {
   const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('');
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+
+    try {
+      const response = await fetch('/api/generate', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({userInput})
+      });
+
+      const data = await response.json();
+      const {output} = data;
+
+      setApiOutput(`${output.text}`);
+      setIsGenerating(false);
+
+    } catch(error) {
+      setIsGenerating(false);
+      return new Error('Something when wrong calling endpoint!')
+    }
+  }
 
   const onUserChangedText = (e) => {
     console.log(e.target.value);
@@ -19,10 +43,10 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>Voltage</h1>
+            <h1>VoltageAI</h1>
           </div>
           <div className="header-subtitle">
-            <h2>Your high power microwave assistant</h2>
+            <h2>Antonio's high power microwave assistant</h2>
           </div>
         </div>
         <div className='prompt-container'>
@@ -32,25 +56,27 @@ const Home = () => {
             value={userInput}
             onChange={(e) => onUserChangedText(e)}/>
           <div className='prompt-buttons'>
-            <a className='generate-button' onClick={null}>
+            <a className={isGenerating ?
+              'generate-button loading' : 'generate-button'}
+              onClick={callGenerateEndpoint}>
               <div className='generate'>
-                <p>Generate</p>
+                {isGenerating ? <span className='loader'></span> : <p>Generate</p>}
               </div>
             </a>
           </div>
         </div>
-      </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+        {apiOutput && (
+          <div className='output'>
+            <div className='output-header-container'>
+              <div className='output-header'>
+                <h3>Output</h3>
+              </div>
+            </div>
+            <div className='output-content'>
+              <p>{apiOutput}</p>
+            </div>
           </div>
-        </a>
+        )}
       </div>
     </div>
   );
